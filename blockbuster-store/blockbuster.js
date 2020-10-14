abb91e230a26439dfcb8e916ba8587d6e854d816
Chris-Bortel <bortelcc19@gmail.com>
2020-10-13 22:50:18 -0700

    wired up everything

--- blockbuster-store/blockbuster.js
+++ blockbuster-store/blockbuster.js
@@ -1,21 +1,72 @@
 'use strict';
 
-
 'use strict';
 const io = require('socket.io-client');
-let host = 'http://localhost:3000'; 
+let host = 'http://localhost:3000';
 const hubConnection = io.connect(host);
 
 function logger(payload) {
-    return payload; 
+  return payload;
 }
 
+let movies = {
+  results: [
+    {
+      name: 'Lord of The Rings',
+      status: 'in stock',
+      checked_out_by: null,
+    },
 
-setInterval(() => {
-    let movies = ['Top Gun','Lord of The Rings', 'Dirty Dancing', 'Mission Impossible', 'Hard to Kill', 'Ghost Rider', 'Ghost Busters', 'Bill and Teds Excellent Adventure'];
+    {
+      name: 'Dirty Dancing',
+      status: 'in stock',
+      checked_out_by: null,
+    },
 
-    let randomMovie = movies[Math.floor(Math.random()*movies.length)];
-    hubConnection.emit('in-stock', randomMovie); 
-},5000);
+    {
+      name: 'Mission Impossible',
+      status: 'in stock',
+      checked_out_by: null,
+    },
 
-hubConnection.on('in-stock', logger); 
+    {
+      name: 'Hard to Kill',
+      status: 'in stock',
+      checked_out_by: null,
+    },
+  ],
+};
+
+hubConnection.on('request', (payload) => {
+  console.log(`Movie: ${payload} requested`);
+  let dbMovie = { status: 'broken', name: 'movie not in database' };
+  let index;
+
+  for (let i = 0; i < movies.results.length; i++) {
+    if (movies.results[i].name === payload) {
+      // console.log(movies.results[i]);
+      dbMovie = movies.results[i];
+      // console.log(dbMovie.status);
+      index = i;
+    } else {
+      // dbMovie = 'movie not in database';
+    }
+  }
+
+  console.log(dbMovie.status);
+  if (dbMovie.status === 'in stock') {
+    hubConnection.emit('check-out', dbMovie);
+    movies.results[index].status = 'checked-out';
+  } else {
+    hubConnection.emit('check-out', 'movie not in stock');
+  }
+});
+
+hubConnection.on('check-in', (payload) => {
+  for (let i = 0; i < movies.results.length; i++) {
+    if (movies.results[i].name === payload) {
+      movies.results[i].status = 'in stock';
+      console.log(payload, 'checked back in');
+    }
+  }
+});
--- hub.js
+++ hub.js
@@ -1,17 +1,18 @@
-
 'use strict';
 const io = require('socket.io')(3000);
 
 io.on('connection', (socket) => {
-    console.log('were connected', socket.id);
-    
-    socket.on('in-stock', (payload) => {   
-        socket.emit('in-stock',payload);
-        console.log(payload, 'is ready to be checked out');
-    });
-    socket.on('checked-out', (payload) => {
-        console.log(payload, 'was checked out');
-        io.emit('checked-out', payload);
-    })
-});
+  console.log('were connected', socket.id);
 
+  socket.on('request', (payload) => {
+    io.emit('request', payload);
+  });
+
+  socket.on('check-out', (payload) => {
+    io.emit('check-out', payload);
+  });
+
+  socket.on('check-in', (payload) => {
+    io.emit('check-in', payload);
+  });
+});
